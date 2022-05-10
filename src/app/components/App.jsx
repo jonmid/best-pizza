0224fb11bcdf087e3046a250b410aec3632054d9
Jhonatan Mideros <10jonmid@gmail.com>
2022-05-10 07:48:48 -0500

    [add] configuracion de colores base de la app

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <h1 class='text-3xl font-bold text-[#f9c444] underline'>App Best Pizza</h1>
+      <h1 className='text-3xl font-bold text-app-primary underline'>App Best Pizza</h1>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        app: {
+          primary: '#f9c444',
+          white: '#FFFFFF',
+          black: '#242229',
+          'gray-black': '#615b64',
+          'gray-light': '#9c949c'
+        }
+      }
+    }
   },
   plugins: []
 }
