1edd522fbd5ded8a5113bc62aa460d205e3c5880
Jhonatan Mideros <10jonmid@gmail.com>
2022-05-10 17:39:35 -0500

    [add] se crea componente de container para manejar las secciones con media queries

---
+++ src/app/components/Container.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './../styles/container.css'
+
+const Container = ({ children }) => {
+  return <main className='main'>{children}</main>
+}
+
+export { Container }
