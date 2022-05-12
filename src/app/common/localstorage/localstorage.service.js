4e40c8cbaedd460fb5e0358038f4ac1acff3f0af
Jhonatan Mideros <10jonmid@gmail.com>
2022-05-12 14:36:40 -0500

    [add] se crea archivo de configuracion para localstorage

---
+++ src/app/common/localstorage/localstorage.service.js
@@ -0,0 +1,40 @@
+const NAME_LOCALSTORAGE_USER = 'STORES_USER'
+const NAME_LOCALSTORAGE_STORES = 'STORES_LIST'
+const NAME_LOCALSTORAGE_BOOKMARKS = 'STORES_BOOKMARKS'
+
+const localStorageUser = localStorage.getItem(NAME_LOCALSTORAGE_USER)
+const localStorageStores = localStorage.getItem(NAME_LOCALSTORAGE_STORES)
+const localStorageBookmarks = localStorage.getItem(NAME_LOCALSTORAGE_BOOKMARKS)
+
+export const ValidateLocalStorage = () => {
+  const data = {
+    user: {},
+    stores: [],
+    bookmarks: []
+  }
+
+  if (localStorageUser) {
+    data.user = JSON.parse(localStorageUser)
+  } else {
+    localStorage.setItem(NAME_LOCALSTORAGE_USER, JSON.stringify({}))
+  }
+
+  if (localStorageStores) {
+    data.stores = JSON.parse(localStorageStores)
+  } else {
+    localStorage.setItem(NAME_LOCALSTORAGE_STORES, JSON.stringify([]))
+  }
+
+  if (localStorageBookmarks) {
+    data.bookmarks = JSON.parse(localStorageBookmarks)
+  } else {
+    localStorage.setItem(NAME_LOCALSTORAGE_BOOKMARKS, JSON.stringify([]))
+  }
+
+  return data
+}
+
+export const SaveUserStoreLocalStorage = (user, stores) => {
+  localStorage.setItem(NAME_LOCALSTORAGE_USER, user)
+  localStorage.setItem(NAME_LOCALSTORAGE_STORES, stores)
+}
