d248bbc0565bd7159cfcefd66f201c44d75cbbcf
Jhonatan Mideros <10jonmid@gmail.com>
2022-05-12 15:17:32 -0500

    [update] se crea funcionalidad del navbar para cambiar entre paginas

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 import './../styles/navbar.css'
 
 const Navbar = () => {
+  const activeStyle = 'navbar__item--active'
+  const noActiveStyle = 'navbar__item--noactive'
+
   return (
     <nav className='navbar'>
       <ul className='navbar__list'>
-        <li className='navbar__list--item'>Pizzerías</li>
-        <li className='navbar__list--item'>Ubicación</li>
-        <li className='navbar__list--item'>Favoritos</li>
+        <li>
+          <NavLink className={({ isActive }) => (isActive ? activeStyle : noActiveStyle)} to='/'>
+            Pizzerías
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink className={({ isActive }) => (isActive ? activeStyle : noActiveStyle)} to='location'>
+            Ubicación
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink className={({ isActive }) => (isActive ? activeStyle : noActiveStyle)} to='bookmarks'>
+            Favoritos
+          </NavLink>
+        </li>
       </ul>
     </nav>
   )
