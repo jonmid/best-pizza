1f486d334d5cc49656b033ab26019392036ade44
Jhonatan Mideros <10jonmid@gmail.com>
2022-05-12 10:22:31 -0500

    [update] se programa funcionalidad en componente Protectedroute para validar si el usuario esta autenticado

--- src/app/components/ProtectedRoute.jsx
+++ src/app/components/ProtectedRoute.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
 
 const ProtectedRoute = ({ children }) => {
+  const isAuthenticated = useSelector(state => state.storeReducer.isAuthenticated)
+
+  if (!isAuthenticated) {
+    return <Navigate to='/auth' /> //redirect
+  }
+
   return children
 }
 
