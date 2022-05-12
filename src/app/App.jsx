b4117e585ea1585dab4eabf1333acce9f07f9256
Jhonatan Mideros <10jonmid@gmail.com>
2022-05-12 08:06:13 -0500

    [update] creacion de componente base para proteccion de rutas y configuracion de rutas de la app

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
-import { Background } from './components/Background'
+import { Route, Routes } from 'react-router-dom'
 
 import { Container } from './components/Container'
+import { Background } from './components/Background'
+import { Home } from './pages/Home'
+import { Pizzerias } from './pages/Pizzerias'
 import { Login } from './pages/Login'
+import { ProtectedRoute } from './components/ProtectedRoute'
 
 function App() {
   return (
     <Container>
       <Background />
 
-      <Login />
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Pizzerias />} />
+        </Route>
+        <Route path='/auth' element={<Login />} />
+        <Route path='*' element={<h1>NotFound</h1>} />
+      </Routes>
     </Container>
   )
 }
