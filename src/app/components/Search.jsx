60fb51416912361a0c10e95bff77712973eee3cd
Jhonatan Mideros <10jonmid@gmail.com>
2022-05-12 18:20:06 -0500

    [update] se ajusta pagina de pizzas y se coloca funcionalidad a componente de busqueda de tiendas

--- src/app/components/Search.jsx
+++ src/app/components/Search.jsx
@@ -1,26 +1,27 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useForm } from 'react-hook-form'
 
 import './../styles/search.css'
 import { Button } from './Button'
 
-const Search = () => {
-  const [textInput, setTextInput] = useState('')
+const Search = ({ setFilter }) => {
+  const { register, handleSubmit } = useForm()
 
-  const handleClickSearch = () => {
-    // TODO: search for the textInput
+  const onSubmit = data => {
+    setFilter(data.search.toLowerCase())
   }
 
   return (
-    <div className='search'>
-      <input type='text' name='search' value={textInput} onChange={e => setTextInput(e.target.value)} placeholder='Buscar tienda ...' className='search__input' />
+    <form className='search' onSubmit={handleSubmit(onSubmit)}>
+      <input type='text' name='search' placeholder='Buscar tienda ...' className='search__input' {...register('search')} />
 
-      <Button type='button' style='button-secondary'>
+      <Button type='submit' style='button-secondary'>
         <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5 duration-300 sm:mr-2' viewBox='0 0 20 20' fill='currentColor'>
           <path fillRule='evenodd' d='M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z' clipRule='evenodd' />
         </svg>
         <span className='hidden duration-300 sm:block'>Buscar</span>
       </Button>
-    </div>
+    </form>
   )
 }
 
--- src/app/pages/Pizzerias.jsx
+++ src/app/pages/Pizzerias.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 
 import './../styles/pizzerias.css'
@@ -6,23 +6,47 @@ import { TitlePage } from '../components/TitlePage'
 import { Search } from '../components/Search'
 import { ListStores } from '../components/ListStores'
 import { ItemStore } from '../components/ItemStore'
+import { Empty } from '../components/Empty'
 
 const Pizzerias = () => {
   const { stores } = useSelector(state => state.storeReducer)
+  const [filter, setFilter] = useState('')
+  let renderView
+
+  if (filter.length > 0) {
+    const filteredStores = stores.filter(item => item.name.toLowerCase().includes(filter))
+    if (filteredStores.length > 0) {
+      renderView = (
+        <ListStores>
+          {filteredStores.map(store => (
+            <ItemStore key={store.id} store={store} />
+          ))}
+        </ListStores>
+      )
+    } else {
+      renderView = <Empty title='No se encontró resultados' message='Verifique la información que ingresó en el campo de búsqueda.' />
+    }
+  }
+
+  if (filter.length === 0) {
+    renderView = (
+      <ListStores>
+        {stores.map(store => (
+          <ItemStore key={store.id} store={store} />
+        ))}
+      </ListStores>
+    )
+  }
 
   return (
     <>
       <TitlePage title='Tiendas' subtitle='Escoge tu pizzería favorita' />
 
       <section className='mt-12'>
-        <Search />
+        <Search setFilter={setFilter} />
       </section>
 
-      <ListStores>
-        {stores.map(item => (
-          <ItemStore key={item.id} store={item} />
-        ))}
-      </ListStores>
+      {renderView}
     </>
   )
 }
