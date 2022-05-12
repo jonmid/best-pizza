71f6c4346414db1ff991be4c8c6ba617b09c01c3
Jhonatan Mideros <10jonmid@gmail.com>
2022-05-11 21:02:14 -0500

    [add] se crea componente para listar las tiendas

---
+++ src/app/components/ListStores.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './../styles/listStores.css'
+
+const ListStores = ({ children }) => {
+  return <ul className='listStores'>{children}</ul>
+}
+
+export { ListStores }
