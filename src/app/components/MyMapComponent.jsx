649590efdf22661ee92b64d3cbe8120265f5506c
Jhonatan Mideros <10jonmid@gmail.com>
2022-05-12 21:13:03 -0500

    [update] se actualiza mark de tienda

--- src/app/components/MyMapComponent.jsx
+++ src/app/components/MyMapComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api'
+import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from '@react-google-maps/api'
 
 const mapContainerStyle = {
   width: '100%',
@@ -11,6 +11,16 @@ const center = {
   lat: 8.99595298238361,
   lng: -79.52112692660712
 }
+
+const MarkComponent = ({ position, text }) => (
+  // <InfoWindow position={position}>
+  //   <div>
+  //     <h1>{text}</h1>
+  //   </div>
+  // </InfoWindow>
+
+  <Marker position={position} label={text} />
+)
 
 const MyMapComponent = () => {
   const { stores } = useSelector(state => state.storeReducer)
@@ -44,9 +54,9 @@ const MyMapComponent = () => {
   }
 
   return isLoaded ? (
-    <GoogleMap id='marker-example' mapContainerStyle={mapContainerStyle} zoom={12} center={center}>
+    <GoogleMap id='InfoWindow-example' mapContainerStyle={mapContainerStyle} zoom={12} center={center}>
       {stores.map(item => (
-        <Marker key={item.id} position={positions[item.name]} label={item.name} />
+        <MarkComponent key={item.id} position={positions[item.name]} text={item.name} />
       ))}
     </GoogleMap>
   ) : (
